be82f00313102a1eede6f9a2e4dc4242255867de
016apih <46413800+016apih@users.noreply.github.com>
2023-05-06 22:17:52 +0700

    add React-Select

---
+++ src/app_components/index.js
@@ -0,0 +1,1 @@
+export { default as SelectComponent } from './form/SelectComponent.js';
---
+++ src/app_pages/main-page.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+import { SelectComponent } from '../app_components';
+
+const MainPage = memo(() => {
+   return (<>
+      <div>MainPage</div>
+      <SelectComponent />
+   </>)
+});
+
+export default MainPage;
