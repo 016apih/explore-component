8f9a1c330cddac799b52ef2dbf2be30f0f99806d
016apih <46413800+016apih@users.noreply.github.com>
2023-05-08 21:29:05 +0700

    add react-bootrap with bootstrap 5.2

--- src/app_pages/main-page.js
+++ src/app_pages/main-page.js
@@ -1,10 +1,36 @@
 import React, { memo } from 'react';
+import { Card } from 'react-bootstrap';
+
 import { SelectComponent } from '../app_components';
 
 const MainPage = memo(() => {
    return (<>
-      <div>MainPage</div>
-      <SelectComponent />
+      <div className="w-50 m-3">
+         <SelectComponent />
+      </div>
+      <div className="row mx-2">
+         <div className="col-md-4">
+            <Card>
+               <Card.Body>
+                  <Card.Title>Form</Card.Title>
+                  <Card.Text>
+                     <SelectComponent />
+                  </Card.Text>
+               </Card.Body>
+            </Card>
+         </div>
+         <div className="col-md-4">
+            <div className="card">
+               <h5 className="card-title px-3 pt-3 text-end text-decoration-underline">
+                  Modal
+               </h5>
+               <div className="card-body">
+                  <SelectComponent />
+               </div>
+            </div>
+         </div>
+
+      </div>
    </>)
 });
 
