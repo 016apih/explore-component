86ad2594f514971e39a179426a469e0e9303b87f
016apih <46413800+016apih@users.noreply.github.com>
2023-05-10 21:52:53 +0700

    update agGrid-page & index.js

--- src/app_pages/tabel-aggrid/aggrid-page.js
+++ src/app_pages/tabel-aggrid/aggrid-page.js
@@ -1,16 +1,48 @@
-import React, { memo } from 'react';
+import React, { memo, useState, Suspense } from 'react';
+import { Form } from 'react-bootstrap';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-import CheckboxWithFirstColumn from './checkbox-with-first-column';
+import { 
+   CheckboxWithFirstColumn,
+   CheckboxWithCustomColumn
+} from './index.js'
+// const { CheckboxWithFirstColumn} = React.lazy(() => import('./index.js'));
+
+const agGridList = [
+   { key: "checkbox1", name: "CheckboxWithFirstColumn", label: "Checkbox 1" },
+   { key: "checkbox2", name: "CheckboxWithCustomeColumn", label: "Checkbox 1" },
+]
 
 const AgGridPage = memo(() => {
+   const [ tableActive, setTableActive ] = useState(agGridList[0])
    return (
-   <div className="row mx-2">
-      <div>AgGridPage</div>
-      <CheckboxWithFirstColumn />
-   </div>
+      <Suspense fallback={<div>Loading...</div>}>
+         <div className="row mx-2">
+            {agGridList.map(d => 
+               <div key={d.key} className="col-auto mb-3">
+                  <Form.Check 
+                     type="radio" 
+                     id={d.key}
+                     label={d.name}
+                     checked={tableActive.key === d.key}
+                     onChange={(e) => setTableActive(d)}
+                  />
+               </div>
+            )}
+         </div>
+         <div className="row mx-2">
+            {tableActive.key === "checkbox1" ? 
+               <CheckboxWithFirstColumn />
+            : tableActive.key === "checkbox2" ?
+               <CheckboxWithCustomColumn />
+            : 
+               ""
+
+            }
+         </div>
+      </Suspense>
    )
 });
 
--- src/app_pages/tabel-aggrid/index.js
+++ src/app_pages/tabel-aggrid/index.js
@@ -1,1 +1,4 @@
 export { default as AgGridPage } from './aggrid-page.js';
+
+export { default as CheckboxWithFirstColumn } from './checkbox-with-first-column.js';
+export { default as CheckboxWithCustomColumn } from './checkbox-with-custom-column.js';
