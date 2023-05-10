ffbb22fe59b552dfb80b1d3fa42f53ecc3deae04
016apih <46413800+016apih@users.noreply.github.com>
2023-05-10 21:09:10 +0700

    update main-page with 2 page

--- src/app_pages/main-page.js
+++ src/app_pages/main-page.js
@@ -1,36 +1,31 @@
-import React, { memo } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { memo, useState } from 'react';
+
+import { FormCard1 } from './form-page';
+import { AgGridPage } from './tabel-aggrid';
 
 import { SelectComponent } from '../app_components';
 
+const options = [
+   { key: "form", value: 'form', label: 'Form' },
+   { key: "agGrid", value: 'agGrid', label: 'Tabel Ag Grid' },
+   // { key: "", value: 'vanilla', label: 'Vanilla' },
+];
+
 const MainPage = memo(() => {
+   const [ pageActive, setPageActive ] = useState(null)
    return (<>
       <div className="w-50 m-3">
-         <SelectComponent />
+         <SelectComponent
+            options={options}
+            onChange={setPageActive}
+         />
       </div>
-      <div className="row mx-2">
-         <div className="col-md-4">
-            <Card>
-               <Card.Body>
-                  <Card.Title>Form</Card.Title>
-                  <Card.Text>
-                     <SelectComponent />
-                  </Card.Text>
-               </Card.Body>
-            </Card>
-         </div>
-         <div className="col-md-4">
-            <div className="card">
-               <h5 className="card-title px-3 pt-3 text-end text-decoration-underline">
-                  Modal
-               </h5>
-               <div className="card-body">
-                  <SelectComponent />
-               </div>
-            </div>
-         </div>
-
-      </div>
+      {pageActive?.value === "form" ?
+            <FormCard1 />
+         : pageActive?.value === "agGrid" ?
+            <AgGridPage />
+         : ""
+      }
    </>)
 });
 
