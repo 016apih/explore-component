0ed7fd9a420bcea56938e5889a6a026b1a9b71a5
016apih <46413800+016apih@users.noreply.github.com>
2023-05-12 15:00:09 +0700

    add theme anychart

--- src/app_pages/anychart/explore/product-comparison-chart.js
+++ src/app_pages/anychart/explore/product-comparison-chart.js
@@ -4,12 +4,19 @@ import 'anychart/dist/css/anychart-ui.min.css'; // wajib ada untuk merapihkan ta
 
 import AnyChart from '/node_modules/anychart-react/dist/anychart-react.min';
 import { get_code_product, get_product_comparison } from '../data';
+import chartThemeList from '../theme';
+
+// default constanta chart
+const defData = [[]]; // array of array
+const defCode = [{saham: "Series"}];  // array of object
+const defTheme = chartThemeList[0]
 
 const ProductComparison = memo(() => {
-   const [chartData, setChartData] = useState([[]]); // array of array
-   const [chartCode, setCartCode] = useState([{saham: "Series"}]) // array of object
+   const [chartData, setChartData] = useState(defData); 
+   const [chartCode, setCartCode] = useState(defCode);
+   const [chartTheme, setCharTheme] = useState(defTheme);
 
-   const chart = anychart.stock(); 
+   const chart = anychart.stock();
 
    // set scroll date
    chart.scroller(true); 
@@ -48,16 +55,19 @@ const ProductComparison = memo(() => {
    }
 
    useEffect(() => {
+      // set theme
+      anychart.theme(chartTheme.theme);
+
       // create dataset
       let datasetObj = {}, mapObj={}, chartObj={};
       chartData.map((d, key) => {
          datasetObj = { ...datasetObj, [`tab${key}`]: anychart.data.table()}
       });
 
-      // mapping dataset [NavDate, Nav, NavUnit]
+      // mapping dataset by index data [NavDate, Nav, NavUnit]
       Object.keys(datasetObj).length > 0 && Object.keys(datasetObj).map((d, id) => {
          datasetObj[d].addData(chartData[id]);
-         mapObj = { ...mapObj, [`mapObj${id}`]: datasetObj[d].mapAs({ 'value': 2 }) } // map by index Data
+         mapObj = { ...mapObj, [`mapObj${id}`]: datasetObj[d].mapAs({ 'value': 2 }) }
       })
       
       // create line series on the plot
@@ -89,22 +99,50 @@ const ProductComparison = memo(() => {
          chartSeries.selected().risingFill(colorSeries, 0.9);
          chartSeries.selected().risingStroke(colorSeries, 0.9);
       });
-   },[ chartData ])
+   },[ chartData, chartTheme ])
 
    const setData = () => {
       setChartData(get_product_comparison());
       setCartCode(get_code_product());
    }
 
-   return (
-      <div>
-         <button onClick={setData}>Set Data</button>
+   const onSelectTheme = (val) => {
+      let newTheme = chartThemeList.find(d => d.value === val);
+      setCharTheme(newTheme)
+   }
+
+   const onResetData = () => {
+      setChartData(defData);
+      setCartCode(defCode);
+      setCharTheme(defTheme);
+   }
+
+   return (<>
+      <div className="row">
+         <div className="col-auto">
+            <button onClick={setData}>Set Data</button>
+         </div>
+         <div className="col-auto">
+            <select placeholder="Theme" 
+               onChange={(e) => onSelectTheme(e.target.value)}
+               value={chartTheme.value}
+            >
+               { chartThemeList.map((d, id) => (
+                  <option key={"cht-theme"+id} value={d.value}>
+                     {d.label}
+                  </option>
+               )) }
+            </select>
+         </div>
+         <div className="col-auto">
+            <button onClick={onResetData}>Reset Data</button>
+         </div>
+      </div>
          <AnyChart 
             instance={chart}
             id="product-comparison-chart"
          />
-      </div>
-   )
+   </>)
 })
 
 export default ProductComparison;
