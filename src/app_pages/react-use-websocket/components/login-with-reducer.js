69f00300f594288d450364726b48824b9cb6334e
016apih <46413800+016apih@users.noreply.github.com>
2023-05-22 12:03:23 +0700

    replace state with ref

--- src/app_pages/react-use-websocket/components/login-with-reducer.js
+++ src/app_pages/react-use-websocket/components/login-with-reducer.js
@@ -1,23 +1,17 @@
-import React, { memo, useState, useEffect } from 'react';
+import React, { memo, useRef } from 'react';
 import { useWs } from '../context/websocket2';
 
 const LoginPage = memo(() => {
    const { sendWs, setVars, vars } = useWs();
-
-   const [username, setUsername] = useState("");
-   const [password, setPassword] = useState("");
+   const username = useRef(null);
+   const password = useRef(null);
    
    const doLogin = () => {
-      setVars({ type: "SET_DO_LOGIN", payload: { user: username } })
-      sendWs({ action_type: 'LOGIN', user: username, password, terminal: "web" })
+      let user = username.current.value;
+      let pwd = password.current.value;
+      setVars({ type: "SET_DO_LOGIN", payload: { user } })
+      sendWs({ action_type: 'LOGIN', user, password: pwd, terminal: "web" })
    }
-
-   useEffect(() => {
-      setUsername("dxtest22");
-      setPassword("password");
-   }, [])
-
-   console.log(vars)
 
    return (<>
       <h3>LoginPage</h3>
@@ -26,16 +20,14 @@ const LoginPage = memo(() => {
       <div className="row">
          <div className="col-auto">
             <label>Username</label>
-            <input type='text' placeholder='username'
-               value={username}
-               onChange={e => setUsername(e.target.value)}
+            <input type='text' placeholder='username' ref={username}
+               defaultValue="dxtest22"
             />
          </div>
          <div className="col-auto">
             <label>Password</label>
-            <input type='password' placeholder='password'
-               value={password}
-               onChange={e => setPassword(e.target.value)}
+            <input type='password' placeholder='password' ref={password}
+               defaultValue="password"
             />
          </div>
          <div className="col-auto">
