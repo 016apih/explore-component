91ae19e917d2353e3f2aa49e44332e0ec490d673
016apih <46413800+016apih@users.noreply.github.com>
2023-05-23 15:00:13 +0700

    add create fastContextGeneric

--- src/app_pages/react-context/fast-context-generic/fast-context-generic.js
+++ src/app_pages/react-context/fast-context-generic/fast-context-generic.js
@@ -1,12 +1,65 @@
 import React, { useRef, useCallback, useContext, useState, useEffect } from "react";
 
 import './fast-contest.css';
-import createFastContext from "../fast-context-generic/createFastContext";
 
-const { Provider, useStore } = createFastContext({
-   first: "",
-   last: ""
-});
+// create initial Context
+function useStoreData(){
+   const store = useRef({
+      first: "", 
+      last: ""
+   });
+
+   const get = useCallback(() => store.current, []);
+
+   const subscribers = useRef(new Set);
+
+   const set = useCallback((value) => {
+      store.current = { ...store.current, ...value };
+      subscribers.current.forEach(callback => callback())
+   }, []);
+
+   const subscribe = useCallback((callback) => {
+      subscribers.current.add(callback);
+      return () => subscribers.current.delete(callback);
+   },[])
+
+   return { get, set, subscribe }
+}
+
+// create context
+const StoreContext = React.createContext(null);
+
+const Provider = ({ children }) => {
+   return (
+      <StoreContext.Provider value={useStoreData()}>
+         { children }
+      </StoreContext.Provider>
+   )
+}
+
+function useStore (selector) {
+   console.log("selector", selector,  typeof(selector))
+   const store = useContext(StoreContext);
+   if(!store){
+      throw new Error("Store not found")
+   }
+
+   /** start */
+   const [state, setState] = useState(() => selector(store.get()));
+   
+   useEffect(() => {
+      return store.subscribe(() => setState(() => selector(store.get())) );
+   }, [])
+   /** end */
+
+   /* fungsi start-end dapat di ubah menjadi ini jika menggunakan react versi 18.
+      const state = useSyncExternalStore(store.subscribe, () =>
+         selector(store.get())
+      );
+   */
+
+   return [state, store.set]
+}
 
 const TextInput = ({ value }) => {
    const [fieldValue, setStore] = useStore(store => store[value]);
--- src/app_pages/react-context/index.js
+++ src/app_pages/react-context/index.js
@@ -1,8 +1,11 @@
 import FastContext from './fast-context/fast-context.js';
+import FastContextGeneric from './fast-context-generic/fast-context-generic.js';
 
 export { default as ReactContextPage } from './react-context-page.js';
 
 export const reactContextList = [
    { key: "fast-contest", name: "FastContext", comp: FastContext },
-
+   { key: "fast-contest-generic", name: "FastContextGeneric", comp: FastContextGeneric }
 ]
+
+export const defTabActive = reactContextList[1];
