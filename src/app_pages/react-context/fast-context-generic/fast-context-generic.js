d77f79836ff987b0f79767dc07ab12fc101c640d
016apih <46413800+016apih@users.noreply.github.com>
2023-05-23 15:19:54 +0700

    update fast-context-generic

--- src/app_pages/react-context/fast-context-generic/fast-context-generic.js
+++ src/app_pages/react-context/fast-context-generic/fast-context-generic.js
@@ -1,65 +1,12 @@
-import React, { useRef, useCallback, useContext, useState, useEffect } from "react";
+import React from "react";
+import createFastContext from "./createFastContext";
 
 import './fast-contest.css';
 
-// create initial Context
-function useStoreData(){
-   const store = useRef({
-      first: "", 
-      last: ""
-   });
-
-   const get = useCallback(() => store.current, []);
-
-   const subscribers = useRef(new Set);
-
-   const set = useCallback((value) => {
-      store.current = { ...store.current, ...value };
-      subscribers.current.forEach(callback => callback())
-   }, []);
-
-   const subscribe = useCallback((callback) => {
-      subscribers.current.add(callback);
-      return () => subscribers.current.delete(callback);
-   },[])
-
-   return { get, set, subscribe }
-}
-
-// create context
-const StoreContext = React.createContext(null);
-
-const Provider = ({ children }) => {
-   return (
-      <StoreContext.Provider value={useStoreData()}>
-         { children }
-      </StoreContext.Provider>
-   )
-}
-
-function useStore (selector) {
-   console.log("selector", selector,  typeof(selector))
-   const store = useContext(StoreContext);
-   if(!store){
-      throw new Error("Store not found")
-   }
-
-   /** start */
-   const [state, setState] = useState(() => selector(store.get()));
-   
-   useEffect(() => {
-      return store.subscribe(() => setState(() => selector(store.get())) );
-   }, [])
-   /** end */
-
-   /* fungsi start-end dapat di ubah menjadi ini jika menggunakan react versi 18.
-      const state = useSyncExternalStore(store.subscribe, () =>
-         selector(store.get())
-      );
-   */
-
-   return [state, store.set]
-}
+const { Provider, useStore } = createFastContext({
+   first: "",
+   last: "",
+});
 
 const TextInput = ({ value }) => {
    const [fieldValue, setStore] = useStore(store => store[value]);
