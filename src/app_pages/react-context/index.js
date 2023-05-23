2e67ff18f1e72a9cc7bb92c0e8c2ccccdae14670
016apih <46413800+016apih@users.noreply.github.com>
2023-05-23 18:38:41 +0700

    add fast-context-with-connector

--- src/app_pages/react-context/index.js
+++ src/app_pages/react-context/index.js
@@ -1,11 +1,13 @@
 import FastContext from './fast-context/fast-context.js';
 import FastContextGeneric from './fast-context-generic/fast-context-generic.js';
+import FastContextWithConnector from './fast-context-with-connector/app.js';
 
 export { default as ReactContextPage } from './react-context-page.js';
 
 export const reactContextList = [
    { key: "fast-contest", name: "FastContext", comp: FastContext },
-   { key: "fast-contest-generic", name: "FastContextGeneric", comp: FastContextGeneric }
+   { key: "fast-contest-generic", name: "FastContextGeneric", comp: FastContextGeneric },
+   { key: "fast-contest-wit-connector", name: "FastContextWithConnector", comp: FastContextWithConnector }
 ]
 
-export const defTabActive = reactContextList[1];
+export const defTabActive = reactContextList[2];
